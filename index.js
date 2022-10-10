e2f32291cac5092e1251afc8d2873da068899db7
Jonathan Chen <jonathanchen1122@gmail.com>
2022-10-10 09:23:21 -0700

    fixing canvas

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const canvas = document.querySelector('cnavas');
+const canvas = document.querySelector('myCanvas');
 const c = canvas.getContext('2d');
 
-canvas.width = 1024;
-canvas.height = 576;
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 
 c.fillRect(0,0, canvas.width,canvas.height);
 
