8b7a94e506c90425f80481e0698daf321a71f682
Jonathan Chen <jonathanchen1122@gmail.com>
2022-10-11 17:11:44 -0700

    box added

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ class Sprite {
         c.fillRect(0,0, canvas.width, canvas.height);
 
         c.fillStyle = "blue"
-        c.fillRect(this.position.x,this.position.y, 50, 110);
+        c.fillRect(this.position.x,this.position.y, this.position.x + 500, this.position.y + 110);
 
         c.fillStyle = "green"
         c.fillRect(canvas.width/2,canvas.widith/2, 20, 30);
