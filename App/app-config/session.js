6b2e9b798afcb645ddc6715e087ad4d6fffb78b3
malmeda <malmeda@mail.sfsu.edu>
2022-12-14 19:55:52 -0800

    sesh

--- App/app-config/session.js
+++ App/app-config/session.js
@@ -3,8 +3,8 @@ const session = require("express-session");
 const sessionInstance = session({
     secret: "abracadabra",
     cookie: { maxAge: 24 * 60 * 60},
-    resave: false,
-    saveUninitialized: true,
+    resave: true,
+    saveUninitialized: false,
 });
 
 module.exports = sessionInstance;
